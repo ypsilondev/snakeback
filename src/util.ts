269ce95aa8473369e09c832d7ada653969b3a952
yJulian <yJulian@outlook.de>
2020-09-30 15:28:07 +0200

    Fixed coin generation offset

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,5 @@
 export class Util{
     static createNewCoin(): {message: string, payload: {x: number, y: number}} {
-        return {message: "Coin generated", "payload": {x: Math.random()*1390, y: Math.random()*790}};
+        return {message: "Coin generated", "payload": {x: Math.random()*1380+10, y: Math.random()*780+10}};
     }
 }
